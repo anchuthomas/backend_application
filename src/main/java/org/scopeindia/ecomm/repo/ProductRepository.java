cf8fcfae7234da1dcfc97c361d543a1eba73875b
Anchu Thomas <59996876+anchuthomas@users.noreply.github.com>
2026-01-21 04:45:54 +0530

    Update ProductRepository.java

--- src/main/java/org/scopeindia/ecomm/repo/ProductRepository.java
+++ src/main/java/org/scopeindia/ecomm/repo/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://a59fa12714b37448ea3e366e2041dcb3-1666809775.ap-south-1.elb.amazonaws.com")
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
