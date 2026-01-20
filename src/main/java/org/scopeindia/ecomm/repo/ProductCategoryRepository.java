ec1c5c4e51f3a8feb0f62620d9e978e071a28d13
Anchu Thomas <59996876+anchuthomas@users.noreply.github.com>
2026-01-21 04:46:20 +0530

    Update ProductCategoryRepository.java

--- src/main/java/org/scopeindia/ecomm/repo/ProductCategoryRepository.java
+++ src/main/java/org/scopeindia/ecomm/repo/ProductCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://a59fa12714b37448ea3e366e2041dcb3-1666809775.ap-south-1.elb.amazonaws.com")
 @RepositoryRestResource(path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
